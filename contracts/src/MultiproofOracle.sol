50b3324e64729abe2d386f2a373c181e9b2138a6
Zach Obront <zobront@gmail.com>
2024-11-15 14:33:19 -0600

    refactor to simpler emergency pause

--- contracts/src/MultiproofOracle.sol
+++ contracts/src/MultiproofOracle.sol
@@ -25,10 +25,10 @@ contract MultiproofOracle is IMultiproofOracle {
     address treasury;
 
     uint256 immutable emergencyPauseThreshold;
-    uint256 immutable emergencyBond;
     uint256 immutable emergencyPauseTime;
-    address[] emergencyPauses;
-    mapping(address pauser => PauseData) public emergencyPauseData;
+    bool emergencyPaused;
+    uint40 emergencyPauseDeadline;
+    Challenge[] emergencyPauseChallenges;
 
     bool emergencyShutdown;
 
@@ -42,14 +42,13 @@ contract MultiproofOracle is IMultiproofOracle {
 
         // set params
         require(_args.treasuryFeePctWad < 1e18, "treasury fee must be less than 100%");
-        proposalBond = _args.proposalBond;                       // sane default: 1 ETH
-        challengeTime = _args.challengeTime;                     // sane default: 1 day
+        proposalBond = _args.proposalBond;                       // sane default: 3 ETH
+        challengeTime = _args.challengeTime;                     // sane default: 12 hours
         proofReward = _args.proofReward;                         // sane default: 1 ETH
         provingTime = _args.provingTime;                         // sane default: 1 day
-        treasuryFeePctWad = _args.treasuryFeePctWad;             // sane default: 20%
+        treasuryFeePctWad = _args.treasuryFeePctWad;             // sane default: 50%
         treasury = _args.treasury;
-        emergencyPauseThreshold = _args.emergencyPauseThreshold; // sane default: 100
-        emergencyBond = _args.emergencyBond;                     // sane default: 100 ETH
+        emergencyPauseThreshold = _args.emergencyPauseThreshold; // sane default: 200
         emergencyPauseTime = _args.emergencyPauseTime;           // sane default: 10 days
 
         // initialize anchor state with Confirmed status
@@ -126,8 +125,6 @@ contract MultiproofOracle is IMultiproofOracle {
     }
 
     function finalize(uint256 blockNum, bytes32 outputRoot, uint256 index) public {
-        require(emergencyPauses.length == 0, "no finalizing when emergency paused");
-
         ProposalData storage proposal = proposals[blockNum][outputRoot][index];
         require(!isFinalized(proposal.state), "proposal already finalized");
 
@@ -160,17 +157,18 @@ contract MultiproofOracle is IMultiproofOracle {
             return;
         }
 
+        // If it was challenged and all proven, we don't need to wait for the deadline.
+        if (successfulProofCount == provers.length) {
+            proposal.state = ProposalState.Confirmed;
+            payable(proposal.proposer).transfer(proposalBond);
+            return;
+        }
+
         require(proposal.deadline < block.timestamp, "deadline not passed");
 
-        // If we make it here, we know:
-        // - the parent was Confirmed
-        // - the proposal is either Unchallenged or Challenged
-        // - the proposal deadline has passed, so it can be processed
+        if (proposal.state == ProposalState.Unchallenged) {
+            require(!emergencyPaused, "no confirming while emergency paused");
 
-        // If the proposal was unchallenged, or challenged and fully proven, it is Confirmed.
-        // In both cases, there are no additional rewards (since if successfulProofCount == provers.length,
-        // provers have taken all add'l rewards directly), so the proposer gets their bond back.
-        if (proposal.state == ProposalState.Unchallenged || successfulProofCount == provers.length) {
             proposal.state = ProposalState.Confirmed;
             payable(proposal.proposer).transfer(proposalBond);
             return;
@@ -184,10 +182,10 @@ contract MultiproofOracle is IMultiproofOracle {
         // We can calculate this cost as `proposalBond * treasuryFeePctWad / 1e18 * emergencyPauseThreshold`.
         uint treasuryFee = proposalBond * treasuryFeePctWad / 1e18;
         uint proposalBondRewards = proposalBond - treasuryFee;
-        uint proofRewards = proofReward * (provers.length - successfulProofCount);
+        uint challangeBondRefund = proofReward * (provers.length - successfulProofCount);
 
         payable(treasury).transfer(treasuryFee);
-        payable(proposal.challenger).transfer(proposalBondRewards + proofRewards);
+        payable(proposal.challenger).transfer(proposalBondRewards + challangeBondRefund);
 
         // If any proofs were proven, we have an on chain bug and need to shut down to address it.
         if (proposal.provenBitmap != 0) {
@@ -201,11 +199,11 @@ contract MultiproofOracle is IMultiproofOracle {
 
     // This function exists to block a Proof of Whale attack.
     // If an attacker is proposing too many false roots, this allows us to emergency pause the contract
-    // by attesting to that at least 100 false roots have already been challenged.
-    // If any of the challenges are proven, the pauser loses their full bond.
+    // by showing that at least `emergencyPauseThreshold` roots have already been challenged.
+    // An attacker is incentivized not to abuse this because they will pay treasury fees for all their challenges,
+    // effectively losing 50% of `emergencyPauseThreshold` bonds to perform the attack.
     function emergencyPause(Challenge[] memory challenges) public payable {
-        require(msg.value == emergencyBond, "incorrect bond amount");
-        require(emergencyPauseData[msg.sender].deadline == 0, "already in emergency pause");
+        require(!emergencyPaused, "already in emergency pause");
         require(challenges.length >= emergencyPauseThreshold, "not enough challenges");
 
         for (uint i = 0; i < challenges.length; i++) {
@@ -213,32 +211,18 @@ contract MultiproofOracle is IMultiproofOracle {
             require(proposals[c.blockNum][c.outputRoot][c.index].state == ProposalState.Challenged, "proposal not confirmed");
         }
 
-        emergencyPauses.push(msg.sender);
-        emergencyPauseData[msg.sender] = PauseData({
-            deadline: uint40(block.timestamp + emergencyPauseTime),
-            challenges: challenges
-        });
+        emergencyPaused = true;
+        emergencyPauseDeadline = uint40(block.timestamp + emergencyPauseTime);
+        emergencyPauseChallenges = challenges;
     }
 
-    function endPause(address pauser) public {
-        require(emergencyPauseData[pauser].deadline > 0, "not in emergency pause");
+    function endPause() public {
+        require(emergencyPaused, "not in emergency pause");
+        require(block.timestamp > emergencyPauseDeadline);
 
-        if (block.timestamp < emergencyPauseData[pauser].deadline) {
-            require(msg.sender == pauser, "deadline not expired, only pauser can end pause");
-        }
-
-        if (allEmergencyPauseChallengesAreUnproven(msg.sender)) {
-            payable(pauser).transfer(emergencyBond);
-        }
-
-        delete emergencyPauseData[msg.sender];
-        for (uint i = 0; i < emergencyPauses.length; i++) {
-            if (emergencyPauses[i] == msg.sender) {
-                emergencyPauses[i] = emergencyPauses[emergencyPauses.length - 1];
-                emergencyPauses.pop();
-                break;
-            }
-        }
+        emergencyPaused = false;
+        delete emergencyPauseDeadline;
+        delete emergencyPauseChallenges;
     }
 
     ///////////////////////////////
@@ -266,11 +250,11 @@ contract MultiproofOracle is IMultiproofOracle {
         return proposals[blockNum][outputRoot][index].state == ProposalState.Confirmed;
     }
 
-    function allEmergencyPauseChallengesAreUnproven(address pauser) public view returns (bool) {
+    function allEmergencyPauseChallengesAreRejected(address pauser) public view returns (bool) {
         uint length = emergencyPauseData[pauser].challenges.length;
         for (uint i = 0; i < length; i++) {
             Challenge memory c = emergencyPauseData[pauser].challenges[i];
-            if (proposals[c.blockNum][c.outputRoot][c.index].provenBitmap != 0) {
+            if (proposals[c.blockNum][c.outputRoot][c.index].state != ProposalState.Rejected) {
                 return false;
             }
         }
